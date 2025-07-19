d5ff6c49d138e40d381a16e274b4b4bb0b3e7e95
kingsley <kinscoco9@gmail.com>
2025-07-19 13:31:42 +0100

    committing my file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Function based view
+from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic.detail import DetailView
+
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'list_books.html', {'books': books})
+
+
+
+# Class based view
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
