a83bc5cc8b7cfdb8d635a356c630499ca6eb2d14
kingsley <kinscoco9@gmail.com>
2025-07-19 14:18:50 +0100

    committing my file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -17,3 +17,11 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
+
+
+    from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
