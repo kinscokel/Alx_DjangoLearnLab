5541bc26a0487ed6e8547b93bf690868cff964e2
kingsley <kinscoco9@gmail.com>
2025-07-19 20:04:22 +0100

    committing my file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,4 +16,12 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+
+]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
 ]
