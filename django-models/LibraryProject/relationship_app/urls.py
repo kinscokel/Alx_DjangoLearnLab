f6edd7470899591d59317cfc9836300eb37f0ef8
kingsley <kinscoco9@gmail.com>
2025-07-20 23:31:16 +0100

    committing my file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -56,3 +56,13 @@ from django.urls import path, include
 urlpatterns = [
     path('relationship_app/', include('relationship_app.urls')),
 ]
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
+]
