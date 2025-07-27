a35889f160fd008e9442c08417294e3e8262e516
kingsley <kinscoco9@gmail.com>
2025-07-27 23:32:43 +0100

    committing my file

---
+++ advanced_features_and_security/LibraryProject/bookshelf/bookshelf/forms.py
@@ -0,0 +1,8 @@
+# bookshelf/forms.py
+
+from django import forms
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(label='Your Name', max_length=100)
+    email = forms.EmailField(label='Your Email')
+    message = forms.CharField(label='Message', widget=forms.Textarea)
