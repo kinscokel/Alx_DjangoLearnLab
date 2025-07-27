42c403412f1af126442ce2fa73012f0c41b8467a
kingsley <kinscoco9@gmail.com>
2025-07-27 21:27:06 +0100

    committing my file

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -77,3 +77,15 @@ urlpatterns = [
     path('articles/<int:pk>/edit/', views.article_edit, name='article_edit'),
     path('articles/<int:pk>/delete/', views.article_delete, name='article_delete'),
 ]
+
+
+# bookshelf/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.book_list, name='book_list'),
+    path('books/create/', views.book_create, name='book_create'),
+    path('books/<int:pk>/edit/', views.book_edit, name='book_edit'),
+    path('books/<int:pk>/delete/', views.book_delete, name='book_delete'),
+]
