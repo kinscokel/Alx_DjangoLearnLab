f36d1ac9cce465fc103c8eb757214cef16d78e8e
kingsley <kinscoco9@gmail.com>
2025-07-27 23:20:26 +0100

    committing my file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -81,3 +81,11 @@ def example_form_view(request):
         'form': form,
         'books': books
     })
+
+
+from django.shortcuts import render
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
