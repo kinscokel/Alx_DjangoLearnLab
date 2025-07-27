a8603e204301476dc055c75676141829fd9a7cbf
kingsley <kinscoco9@gmail.com>
2025-07-27 23:00:27 +0100

    committing my file

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,13 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
+
+
+# bookshelf/forms.py
+
+from django import forms
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(label='Your Name', max_length=100)
+    email = forms.EmailField(label='Your Email')
+    message = forms.CharField(label='Message', widget=forms.Textarea)
