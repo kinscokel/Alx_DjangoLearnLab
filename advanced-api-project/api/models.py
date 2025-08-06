2854a3937492bbb3f798a61edcf5e5c1dc02b040
kingsley <kinscoco9@gmail.com>
2025-08-06 21:43:47 +0100

    committing my file

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from datetime import datetime
+
+# Author model to represent book authors
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Book model to represent books written by authors
+# Establishes a ForeignKey (many-to-one) relationship to Author
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
