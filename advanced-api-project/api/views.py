afc499fa5ba21f15125368f1b86f0257cf3aa41b
kingsley <kinscoco9@gmail.com>
2025-08-07 23:25:53 +0100

    committing my file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
-
+from rest_framework import generics
+from django_filters import rest_framework
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
