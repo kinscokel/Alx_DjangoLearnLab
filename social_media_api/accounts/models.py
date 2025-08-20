87a823b4c1aef455d24ee3836726fb0fe6f7df10
kingsley <kinscoco9@gmail.com>
2025-08-20 16:55:39 +0100

    committing my file

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -7,8 +7,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    followers = models. ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
-
-    def _str_(self):
-        return self.username
+def __str__(self):
+    return self.username
