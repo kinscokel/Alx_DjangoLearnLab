81e189744a74bc0370d0e6ae61be35bc29c3c196
kingsley <kinscoco9@gmail.com>
2025-08-22 11:51:34 +0100

    commtting task2

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -14,3 +14,12 @@ from .views import UserListView
 urlpatterns = [
     path('users/', UserListView.as_view(), name='user-list'),
 ]
+
+
+from django.urls import path
+from .views import follow_user, unfollow_user
+
+urlpatterns = [
+    path('follow/<int:user_id>/', follow_user, name='follow_user'),
+    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow_user'),
+]
