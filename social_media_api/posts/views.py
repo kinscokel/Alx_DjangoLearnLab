d58f1deb80474e1db5ff4ca9a0d9be592b2bfb42
kingsley <kinscoco9@gmail.com>
2025-08-22 11:48:22 +0100

    commtting task2

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -26,3 +26,21 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
+
+class FeedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        following_users = user.following.all()  # <- this is required
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')  # <- also required
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
